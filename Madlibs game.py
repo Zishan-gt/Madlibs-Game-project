7b4ae3fe7b4ed28db0d309a7eb044bc579a04588
Zishan <zishanansari00000123@gmail.com>
2026-07-26 12:29:14 +0530

    Add files via upload

---
+++ Madlibs game.py
@@ -0,0 +1,14 @@
+#Madlibs game (Complete the sentence with appropriate words)
+
+
+adjective1 = input("Enter an adjective:(description)")
+noun1 = input("Enter a noun:(person, place, or thing)")
+adjective2 = input("Enter another adjective:(description)")
+verb1 = input("Enter a verb:(end with ing)")
+adjective3 = input("Enter one more adjective:(description)")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
